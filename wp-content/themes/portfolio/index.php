4f73e11303d45db3066910fc8c1f887dd27c2cfc
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-03-29 10:41:57 +0100

    Creating landing page

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,13 +1,13 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio de Delvenne Gauthier</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+if(have_posts()): while(have_posts()): the_post(); ?>
+
+    <h2><?= get_the_title(); ?></h2>
+
+    <div><?= get_the_content(); ?></div>
+
+<?php
+    // On ferme "la boucle" (The Loop):
+endwhile; else: ?>
+    <p>La page est vide.</p>
+<?php endif; ?>
