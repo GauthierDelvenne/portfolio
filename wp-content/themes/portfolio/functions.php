6c9c50e95bbd28c57356ed7b1d8ef8c39f6ce78b
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-03-29 11:08:06 +0100

    clean WP

---
+++ wp-content/themes/portfolio/functions.php
@@ -0,0 +1,14 @@
+<?php
+include_once('acf.php');
+
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+include_once __DIR__ . '/disable-gutenberg.php';
+include_once __DIR__ . '/remove-default-actions.php';
+include_once __DIR__ . '/vite-manifest.php';
+
+
+
+
---
+++ wp-content/themes/portfolio/remove-default-actions.php
@@ -0,0 +1,12 @@
+<?php
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'rest_output_link_wp_head');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'rsd_link');
+
+
+
