a7ff2be877a8b72e2137b6525bc4125cbb7f5390
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-03-30 19:30:03 +0200

    Update footer.php

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,11 +1,21 @@
 </main>
-<footer>
-    <?php wp_nav_menu([
-        'theme_location' => 'footer',
-        'container' => 'nav',
-    ]); ?>
-    <p>© <?= get_bloginfo('name'); ?></p>
-
+<footer class="footer">
+    <nav class="footer_nav">
+        <h2 class="footer_title">Navigation</h2>
+        <ul class="footer_nav_container">
+            <?php foreach (dw_get_navigation_links('footer') as $link): ?>
+                <li class="nav_item">
+                    <a href="<?= $link->href; ?>" class="nav_link"><?= $link->label; ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+    <section class="contact_details">
+        <ul></ul>
+    </section>
+    <section class="networks">
+        <ul></ul>
+    </section>
     <?php wp_footer(); ?>
 </footer>
 </body>
