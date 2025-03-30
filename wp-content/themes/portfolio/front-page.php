f38a3a38da47e9242a0c1227d81b9b8b99807123
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-03-30 16:10:18 +0200

    Making Front-page using flexible content

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -1,6 +1,26 @@
 <?php get_header(); ?>
-<section>
+<?php if (have_rows('presentation')): ?>
+    <?php while (have_rows('presentation')) : the_row(); ?>
+        <?php if (get_row_layout() == 'presentation_me'): ?>
+            <section class="presentation">
+            <h2 class="hidden">Présentation de ma personne</h2>
+            <div class="presentation_me">
+                <figure class="presentation_fig">
 
-<h2>test</h2>
-</section>
+                </figure>
+                <p class="presentation_firstname"><?php the_sub_field('firstname'); ?></p>
+                <p class="presentation_lastname"><?php the_sub_field('lastname'); ?></p>
+                <p class="presentation_profession"><?php the_sub_field('profession'); ?></p>
+            </div>
+        <?php elseif (get_row_layout() == 'presentation_about'): ?>
+            <div class="presentation_about">
+                <p class="presentation_about_description"><?php the_sub_field('description'); ?></p>
+                <div class="presentation_about_illustration">
+                    <p lang="en"><?php the_sub_field('context_message'); ?></p>
+                </div>
+            </div>
+            </section>
+        <?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
