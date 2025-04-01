553dc997524628c293e7229935489fa85a871de5
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-04-01 19:02:35 +0200

    Update template-contact.php

--- wp-content/themes/portfolio/template-contact.php
+++ wp-content/themes/portfolio/template-contact.php
@@ -22,35 +22,35 @@
             <fieldset class="form__fields">
                 <div class="field">
                     <label for="lastname" class="field__label">Nom</label>
-                    <input type="text" name="lastname" id="lastname" class="field__input">
+                    <input type="text" name="lastname" id="lastname" class="field__input" placeholder="Votre nom">
                     <?php if (isset($errors['lastname'])): ?>
                         <p class="field__error"><?= $errors['lastname']; ?></p>
                     <?php endif ?>
                 </div>
                 <div class="field">
                     <label for="email" class="field__label">Adresse mail</label>
-                    <input type="email" name="email" id="email" class="field__input">
+                    <input type="email" name="email" id="email" class="field__input" placeholder="Votre adresse mail">
                     <?php if (isset($errors['email'])): ?>
                         <p class="field__error"><?= $errors['email']; ?></p>
                     <?php endif ?>
                 </div>
                 <div class="field">
                     <label for="phone" class="field__label">Numéro de téléphone</label>
-                    <input type="tel" name="phone" id="phone" class="field__input">
+                    <input type="tel" name="phone" id="phone" class="field__input" placeholder="Votre numéro de téléphone">
                     <?php if (isset($errors['phone'])): ?>
                         <p class="field__error"><?= $errors['phone']; ?></p>
                     <?php endif ?>
                 </div>
                 <div class="field">
                     <label for="subject" class="field__label">Sujet</label>
-                    <input type="text" name="subject" id="subject" class="field__input">
+                    <input type="text" name="subject" id="subject" class="field__input" placeholder="Votre sujet">
                     <?php if (isset($errors['subject'])): ?>
                         <p class="field__error"><?= $errors['subject']; ?></p>
                     <?php endif ?>
                 </div>
                 <div class="field">
                     <label for="message" class="field__label">Message</label>
-                    <textarea name="message" id="message" class="field__input"></textarea>
+                    <textarea name="message" id="message" class="field__input" placeholder="Votre texte"></textarea>
                     <?php if (isset($errors['message'])): ?>
                         <p class="field__error"><?= $errors['message']; ?></p>
                     <?php endif ?>
