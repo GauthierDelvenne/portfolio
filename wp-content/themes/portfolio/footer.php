6d27bf38e4dd1672188f1c383212587b0bf382c6
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-04-12 21:06:29 +0200

    fix footer html

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer class="footer">
     <nav class="footer_nav">
-        <h2 class="footer_title">Navigation</h2>
+        <h2 class="footer_nav_title">Navigation</h2>
         <ul class="footer_nav_container">
             <?php foreach (dw_get_navigation_links('footer') as $link): ?>
                 <li class="nav_item">
@@ -10,7 +10,17 @@
             <?php endforeach; ?>
         </ul>
     </nav>
+    <section class="networks">
+        <h2 class="footer_title">Réseaux</h2>
+
+        <ul class="list">
+            <li class="list_item">Github</li>
+            <li class="list_item">Linkedin</li>
+        </ul>
+    </section>
     <section class="contact_details">
+        <h2 class="footer_title">Coordonnées</h2>
+
         <ul class="list">
             <li class="list_item">+32 497 54 69 43</li>
             <li class="list_item">gauthier.delvenne@student.hepl.be</li>
@@ -18,12 +28,7 @@
             <li class="list_item">4160 Anthisnes</li>
         </ul>
     </section>
-    <section class="networks">
-        <ul class="list">
-            <li class="list_item">Github</li>
-            <li class="list_item">Linkedin</li>
-        </ul>
-    </section>
+
     <?php wp_footer(); ?>
 </footer>
 </body>
