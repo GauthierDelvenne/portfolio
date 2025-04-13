8869644e1e79f8ac527146a3378da2f2327d054b
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-04-13 20:36:58 +0200

    add image front page

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -3,11 +3,11 @@
     <?php while (have_rows('presentation')) : the_row(); ?>
         <?php if (get_row_layout() == 'presentation_me'): ?>
             <section class="presentation">
-            <h2 class="hidden">Présentation de ma personne</h2>
-            <div class="presentation_me">
-                <p class="presentation_me_hook"><?php the_sub_field('presentation_title'); ?></p>
-                <p class="presentation_me_profession"><?php the_sub_field('presentation_profession'); ?></p>
-            </div>
+                <h2 class="hidden">Présentation de ma personne</h2>
+                <div class="presentation_me">
+                    <p class="presentation_me_hook"><?php the_sub_field('presentation_title'); ?></p>
+                    <p class="presentation_me_profession"><?php the_sub_field('presentation_profession'); ?></p>
+                </div>
             </section>
         <?php endif; ?>
     <?php endwhile; ?>
@@ -15,29 +15,26 @@
 <?php if (have_rows('discover')): ?>
     <?php while (have_rows('discover')) : the_row(); ?>
         <?php if (get_row_layout() == 'discover_content'): ?>
-    <section class="discover">
-        <div class="discover_content">
-            <h2><?php the_sub_field('discover_title_h2'); ?></h2>
-            <p><?php the_sub_field('discover_content'); ?></p>
-        </div>
-
-        <div class="discover_projects">
+            <section class="discover">
+            <div class="discover_content">
+                <h2><?php the_sub_field('discover_title_h2'); ?></h2>
+                <p><?php the_sub_field('discover_content'); ?></p>
+            </div>
+            <div class="discover_projects">
         <?php elseif (get_row_layout() == 'discover_project'): ?>
             <article class="discover_project">
+                <a href="<?= get_sub_field('discover_link')['url']; ?>"></a>
                 <h3><?php the_sub_field('discover_title_h3'); ?></h3>
-                <figure class="discover_fig">
-                    <!--ajouter l'image-->
-                </figure>
+                <?= responsive_image(get_sub_field('discover_img'), ['lazy' => 'true', 'classes' => 'stage__image']) ?>
             </article>
         <?php elseif (get_row_layout() == 'discover_project_2'): ?>
             <article class="discover_project">
+                <a href="<?= get_sub_field('discover_link')['url']; ?>"></a>
                 <h3 lang="la"><?php the_sub_field('discover_title_h3'); ?></h3>
-                <figure class="discover_fig">
-                    <!--ajouter l'image-->
-                </figure>
+                <?= responsive_image(get_sub_field('discover_img'), ['lazy' => 'true', 'classes' => 'stage__image']) ?>
             </article>
-        </div>
-    </section>
+            </div>
+            </section>
         <?php endif; ?>
     <?php endwhile; ?>
 <?php endif; ?>
