20c772fa199e1914a055d2a871290d2759c4a75b
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-05 19:29:58 +0200

    fix details

--- wp-content/themes/portfolio/project-post.php
+++ wp-content/themes/portfolio/project-post.php
@@ -1,13 +1,14 @@
 <?php
-
 register_post_type('project', [
     'label' => 'Projet',
     'description' => 'Mes projets réalisés',
     'public' => true,
+    'has_archive' => true,
     'menu_position' => 6,
     'menu_icon' => 'dashicons-portfolio',
-    'rewrite' => [
-        'slug' => 'projet',
-    ],
+    'rewrite' => ['slug' => 'projet'],
     'supports' => ['title', 'editor', 'excerpt', 'thumbnail'],
+    'show_in_rest' => true,
+
 ]);
+
