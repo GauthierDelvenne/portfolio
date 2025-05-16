3653bbe2c8e1504989dd43fe4d5adec55f2491d1
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-16 11:08:59 +0200

    Update schema-org.php

--- wp-content/themes/portfolio/functions/schema-org.php
+++ wp-content/themes/portfolio/functions/schema-org.php
@@ -9,19 +9,37 @@ function ajouter_schema_portfolio(): void
               "@type": "Person",
               "name": "Gauthier Delvenne",
               "url": "<?php echo esc_url(home_url()); ?>",
-              "logo": "<?php echo get_template_directory_uri(); ?>/resources/img/black-mascotte.svg",
-          "jobTitle": "Designer / Développeur Web",
-          "image": "https://portfolio.gauthier-delvenne.be/wp-content/uploads/2025/04/picture-me.webp",
-          "description": "Portfolio de Gauthier Delvenne, designer et développeur web basé en Belgique.",
-          "sameAs": [
-            "https://www.linkedin.com/in/gauthier-delvenne",
-            "https://github.com/GauthierDelvenne"
-          ],
-          "worksFor": {
-            "@type": "Organization",
-            "name": "Haute École de la Province de Liège"
-          }
-        }
+      "jobTitle": "Designer / Développeur Web",
+      "image": "https://portfolio.gauthier-delvenne.be/wp-content/uploads/2025/04/picture-me.webp",
+      "description": "Portfolio de Gauthier Delvenne, designer et développeur web basé en Belgique.",
+      "sameAs": [
+        "https://www.linkedin.com/in/gauthier-delvenne",
+        "https://github.com/GauthierDelvenne"
+      ],
+      "worksFor": {
+        "@type": "Organization",
+        "name": "Haute École de la Province de Liège"
+      }
+    }
+        </script>
+
+        <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Portfolio de Gauthier Delvenne",
+              "url": "<?php echo esc_url(home_url()); ?>",
+      "logo": "<?php echo get_template_directory_uri(); ?>/resources/img/mascotte.png"
+    }
+        </script>
+
+        <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Delvenne Gauthier – Développeur Web",
+              "url": "<?php echo esc_url(home_url()); ?>"
+    }
         </script>
         <?php
     }
